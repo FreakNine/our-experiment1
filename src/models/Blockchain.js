1e52b88aa8153b921fcbace6b5cf28bdaab3fd5b
FreakNine <2605972947@qq.com>
2023-05-07 21:28:53 +0800

    创建 Blockchain.js

---
+++ src/models/Blockchain.js
@@ -0,0 +1,26 @@
+// Blockchain
+class Blockchain {
+  // 1. 完成构造函数及其参数
+  /* 构造函数需要包含 
+      - 名字
+      - 创世区块
+      - 存储区块的映射
+  */
+ //构造函数，区块链的名字
+ constructor(name,genesis){
+  this.name=name;
+  this.Blockchain=[this.genesis]
+ }
+
+  // 2. 定义 longestChain 函数
+  /* 
+    返回当前链中最长的区块信息列表
+  */
+  longestChain() {
+  h
+
+    return []
+  }
+}
+
+export default Blockchain
