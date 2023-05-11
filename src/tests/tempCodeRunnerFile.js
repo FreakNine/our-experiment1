d041c6615421e44487253a7589b192811c438274
FreakNine <2605972947@qq.com>
2023-05-12 00:42:06 +0800

    创建 tempCodeRunnerFile.js

---
+++ src/tests/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+  // 区块检查
+  console.assert(longestChain.length == 3, 'Block height should be 2')
+  console.assert(
+    aa.hash == thirdBlock.hash,
+    `Height block hash should be ${thirdBlock.hash}`,
+  )
