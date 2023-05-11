f4e5fedff86fb6d1210fa8c901caf4439c6a327b
FreakNine <2605972947@qq.com>
2023-05-11 23:39:25 +0800

    更新 Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -5,6 +5,10 @@ class Block {
   Blockchain所在的区块链,prehash上一个区块的hash值,height所在链的长度,hash这个区块的hash值。
   */
   
+  Blockchain;//区块所在链的信息
+  prehash;//上一个区块的hash
+  height;//区块的高度
+  hash;//本身的hash值
   constructor(Blockchain,prehash,height,hash) {
     this.height=height;
     this.Blockchain=Blockchain;
