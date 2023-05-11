77a32b089d4f2118d8c32156ba1c5ef178aaca47
FreakNine <2605972947@qq.com>
2023-05-11 23:39:32 +0800

    创建 test.js

---
+++ src/models/test.js
@@ -0,0 +1,8 @@
+
+import Block from "./Block.js";
+import Blockchain from "./Blockchain.js";
+
+var newblockchain =new Blockchain('newchain');
+var newBlock = new Block(newblockchain, 'root', 0, 'root');
+newblockchain.blocks.set(asd,123)
+console.log(Blockchain.blocks)
