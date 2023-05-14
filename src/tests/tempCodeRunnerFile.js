52d75a1193c1d3afb52ff14296752a2bf14d422d
FreakNine <2605972947@qq.com>
2023-05-14 15:03:41 +0800

    更新 tempCodeRunnerFile.js

--- src/tests/tempCodeRunnerFile.js
+++ src/tests/tempCodeRunnerFile.js
@@ -1,6 +1,2 @@
-  // 区块检查
-  console.assert(longestChain.length == 3, 'Block height should be 2')
-  console.assert(
-    aa.hash == thirdBlock.hash,
-    `Height block hash should be ${thirdBlock.hash}`,
-  )
+
+  // console.log(longestChain)
