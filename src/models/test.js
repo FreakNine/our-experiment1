ebbf53a3a5299a7dd27af210b4e8af00e1124687
FreakNine <2605972947@qq.com>
2023-05-14 11:31:01 +0800

    更新 test.js

--- src/models/test.js
+++ src/models/test.js
@@ -1,9 +1,14 @@
+export const num1 = 2
 
 import Block from "./Block.js";
 import Blockchain from "./Blockchain.js";
+import { sha256 } from "crypto.js";
 
 var newblockchain =new Blockchain('newchain');
-var newBlock = new Block(newblockchain, 'root', 0, 'root');
-newblockchain.blocks.set(asd,123)
-console.log(Blockchain.blocks)
-a = newblockchain.longestChain();
+var newBlock = new Block(
+    newblockchain,
+     'root',
+      0,
+      sha256(new Date().getTime().toString()).toString(),
+      );
+console.log(sha256(new Date().getTime().toString()).toString()==num1)
