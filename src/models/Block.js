0f6a65e967969f65e412f186f686fba33fea1292
FreakNine <2605972947@qq.com>
2023-05-14 11:30:57 +0800

    更新 Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,4 +1,4 @@
-
+export const DIFFICULTY = 2
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
@@ -9,11 +9,26 @@ class Block {
   prehash;//上一个区块的hash
   height;//区块的高度
   hash;//本身的hash值
+  Nonce;//区块的随机数
   constructor(Blockchain,prehash,height,hash) {
     this.height=height;
     this.Blockchain=Blockchain;
-    this.prehash=prehash
+    this.prehash=prehash;
     this.hash =hash;
+  }
+
+  //修改区块的随机数。
+  setNonce(Nonce){
+    this.Nonce=Nonce;
+  }
+
+  //验证区块是否符合挖矿难度
+  isValid() {
+    if (this.hash==DIFFICULTY){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
 export default Block
