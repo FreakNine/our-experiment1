00ec4de23c268f95e3aced8a74ef7348c3227e98
FreakNine <2605972947@qq.com>
2023-05-14 14:30:45 +0800

    更新 lesson1.js

--- src/tests/lesson1.js
+++ src/tests/lesson1.js
@@ -55,6 +55,10 @@ const main = () => {
   blockchain.blocks.set(thirdBlock.hash,thirdBlock)
 
   longestChain = blockchain.longestChain()
+<<<<<<< Updated upstream
+=======
+ 
+>>>>>>> Stashed changes
 
   // 区块检查
   console.assert(longestChain.length == 3, 'Block height should be 2')
@@ -62,6 +66,11 @@ const main = () => {
     longestChain[2].hash == thirdBlock.hash,
     `Height block hash should be ${thirdBlock.hash}`,
   )
+<<<<<<< Updated upstream
 }
 
+=======
+  console.log(longestChain)
+  }
+>>>>>>> Stashed changes
 main()
