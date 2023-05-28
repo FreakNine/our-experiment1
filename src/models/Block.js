05dfc4b9df3ea6b3ccb6826f6e0592d7ccf70024
FreakNine <2605972947@qq.com>
2023-05-28 13:05:19 +0800

    更新 Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,4 +1,6 @@
-export const DIFFICULTY = 2
+import UTXOPool from "./UTXOPool.js";
+
+export const DIFFICULTY = 10
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
@@ -12,19 +14,18 @@ class Block {
   height;
   //本身的hash值
   hash;
-  //
+  //剩余资产表(挖出这个区块的奖励时12.5 btc)
   utxoPool;
-  //一个区块奖励最终的受益者
+  //一个区块奖励的最总受益者
   coinbaseBeneficaiary;
-  //矿工
-  miner;
 
   constructor(Blockchain,prehash,height,hash,miner) {
     this.height=height;
     this.Blockchain=Blockchain;
     this.prehash=prehash
     this.hash =hash;
-    this.miner=miner;
+    this.coinbaseBeneficaiary=miner;
+    this.utxoPool = new UTXOPool(miner);
   }
   //验证是否符合挖矿难度
   isValid() {
